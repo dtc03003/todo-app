33fc1332b649a0d7e7decae5b4721826222f58f2
김동현 <dtc03003@naver.com>
2023-02-25 22:01:32 +0900

    todolist 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 const App = () => {
-  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>
+  return(
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList />
+    </TodoTemplate>
+  )
 };
 
 export default App;
